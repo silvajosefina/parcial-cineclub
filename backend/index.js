8a83617f7e007242e330960349c8b391e7b3ba55
Josefina Silva <silvajosefinad02@gmail.com>
2026-08-15 11:56:24 -0300

    agrego lectura de JSON y logging

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3001
 
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`)
+    next()
+})
+
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`)
 })
