3a7a0d548ac35f2f54f436933b2514a03eb86042
Josefina Silva <silvajosefinad02@gmail.com>
2026-08-17 23:30:41 -0300

    configuro CORS para conectar frontend y backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 const PORT = process.env.PORT || 3001
 
 const reviews = []
 
+app.use(cors())
 app.use(express.json())
 
 app.use((req, res, next) => {
